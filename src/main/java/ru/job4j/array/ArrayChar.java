0ca207da97a39a64cd64bd0684389dfb72d31636
lehade <alexey.denisenko@me.com>
2024-05-23 23:43:00 +0300

    6.4.0. Слово начинается с ... [#41585 #496943]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,14 +4,12 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-
         for (int i = 0; i < prefix.length; i++) {
             if (word[i] != prefix[i]) {
                 result = false;
                 break;
             }
         }
-
         return result;
     }
 }
