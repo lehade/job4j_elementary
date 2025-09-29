0d3f8f291cff43c71e5148169d9ee690be206c2f
lehade <alexey.denisenko@me.com>
2025-09-29 22:37:41 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,44 +8,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point firstPoint = new Point(0, 0);
+        Point secondPoint = new Point(2, 0);
+        double out = firstPoint.distance(secondPoint);
         assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
     public void whenM11to22then3dot16() {
         double expected = 3.16;
-        int x1 = -1;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point firstPoint = new Point(-1, 1);
+        Point secondPoint = new Point(2, 2);
+        double out = firstPoint.distance(secondPoint);
         assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
     public void when0M2to44then7dot21() {
         double expected = 7.21;
-        int x1 = 0;
-        int y1 = -2;
-        int x2 = 4;
-        int y2 = 4;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point firstPoint = new Point(0, -2);
+        Point secondPoint = new Point(4, 4);
+        double out = firstPoint.distance(secondPoint);
         assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
     public void when1010toM10M10then28dot28() {
         double expected = 28.28;
-        int x1 = 10;
-        int y1 = 10;
-        int x2 = -10;
-        int y2 = -10;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point firstPoint = new Point(10, 10);
+        Point secondPoint = new Point(-10, -10);
+        double out = firstPoint.distance(secondPoint);
         assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
